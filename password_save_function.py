7ba98e823dcfe1f7190fd7e72164b79126597787
Corey Grainger <corey.grainger@my.jcu.edu.au>
2024-02-15 10:32:04 +1000

    Add password database file and password save function

--- password_save_function.py
+++ password_save_function.py
@@ -1,4 +1,18 @@
 """
 CP2414 Password Save Function
-
 """
+
+import csv
+
+
+def create_password_file_entry(username: str, password: str, salt: int):
+    with open('password_database.csv', 'a') as out_file:
+        password_writer = csv.writer(out_file, )
+        password_writer.writerow([username, password, salt])
+
+
+def test_add_hashed_password():
+    create_password_file_entry('Corey', "gggasdljdksfkjsnvjksa", 66)
+
+
+test_add_hashed_password()
