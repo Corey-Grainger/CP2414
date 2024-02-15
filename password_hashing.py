5e49afe2c613ab7f83ea2f287f114c507202bf8f
Corey Grainger <corey.grainger@my.jcu.edu.au>
2024-02-15 22:05:44 +1000

    Add additional functionality to password checker menu

--- password_hashing.py
+++ password_hashing.py
@@ -12,3 +12,13 @@ def cross_reference_hashed_password(password, hashed_password):
     byte_password = password.encode('utf-8')
     return bcrypt.checkpw(byte_password, bytes(hashed_password, 'utf-8'))
 
+
+def test_bcrypt():
+    salt = bcrypt.gensalt()
+    test_password = 'testpassword123@'
+    byte_password = test_password.encode('utf-8')
+    test_hashed_password = bcrypt.hashpw(byte_password, salt)
+    print(bcrypt.checkpw(bytes(test_password, 'utf-8'), test_hashed_password))
+
+
+test_bcrypt()
--- password_save_function.py
+++ password_save_function.py
@@ -8,8 +8,8 @@ import pandas as pd
 
 def create_password_file_entry(username, password, salt):
     with open('password_database.csv', 'a', encoding='utf-8') as out_file:
-        password_writer = csv.writer(out_file)
-        password_writer.writerow([username, password, salt])
+        password_writer = csv.writer(out_file, delimiter=",")
+        password_writer.writerow([username,password,salt])
 
 def remove_password_file_entry(username):
     df = pd.read_csv('password_database.csv', encoding='utf-8')
