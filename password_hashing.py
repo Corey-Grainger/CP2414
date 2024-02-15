bd62dbca3e2a968dc74b450430a63c8af24988ab
dylan-perk04 <dylan.perkins@my.jcu.edu.au>
2024-02-15 10:44:35 +1000

    Implement function to cross-reference hashed password

--- password_hashing.py
+++ password_hashing.py
@@ -5,4 +5,10 @@ def hash_password(password):
     salt = bcrypt.gensalt()
     byte_password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(byte_password, salt)
+    return hashed_password, salt
+
+
+def cross_reference_hashed_password(password, salt):
+    byte_password = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(byte_password, salt)
     return hashed_password
